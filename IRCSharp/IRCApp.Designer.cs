3ca8cf2b504496a22ccaf844361a66425759e320
yodadude2003 <yodadude2003@gmail.com>
2018-04-18 12:44:07 -0500

    Add project files.

---
+++ IRCSharp/IRCApp.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace IRCSharp
+{
+    partial class IRCApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // IRCApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 471);
+            this.Name = "IRCApp";
+            this.Text = "IRCsharp";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
